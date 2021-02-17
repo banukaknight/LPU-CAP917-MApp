44deb73105e80517522bdccca51a747c557ed1b8
Banuka <banukaknight@gmail.com>
2021-02-17 21:54:11 +0530

    test1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//import routing components by bk
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+
+
+const routes: Routes = [
+  //routing by bk
+  { path: '', component: HomeComponent },
+  { path: 'Home', component: HomeComponent },
+	{ path: 'Login', component: LoginComponent },
+	{ path: 'Register', component: RegisterComponent },
+	{ path: 'About', component: AboutComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [ HomeComponent, LoginComponent, RegisterComponent, AboutComponent ];
